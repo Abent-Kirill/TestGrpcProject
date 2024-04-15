8503cab0ed075aaa46ae4d28d4d87fe3173d0b0b
Кирилл Абент <abent.ka@avgd.pro>
2024-04-15 16:11:36 +0400

    Add project files.

---
+++ Server/Services/GreeterService.cs
@@ -0,0 +1,13 @@
+using Grpc.Core;
+
+namespace Server.Services;
+public class GreeterService(ILogger<GreeterService> logger) : Greeter.GreeterBase
+{
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = $"Hello {request.Name}"
+        });
+    }
+}
